7c2b8536829bd01af8739c4b04940fb612217f2c
zjy <807982261@qq.com>
2020-07-11 11:53:25 +0800

    first commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhangjiyu
+ * @create 2020-07-11 11:47
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
